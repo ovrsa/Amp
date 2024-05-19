7ec046c1abab97a269560a40dc725b2b66bf514b
ovrsa <sufferin.in@gmail.com>
2024-05-19 19:35:16 +0900

    feat/PRI-117-markup-home

--- amp-client/vite.config.ts
+++ amp-client/vite.config.ts
@@ -1,7 +1,17 @@
-import vue from '@vitejs/plugin-vue';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
